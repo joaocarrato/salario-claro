a06e28716db855eab5a27251d3211b5b2e96d9a3
joaocarrato <joaocarrato18@gmail.com>
2026-04-27 16:01:43 -0300

    setup tab navigation with screens for simulator, compare, and history

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,60 @@
+import { useSafeArea } from "@/src/hooks/useSafeArea";
+import Octicons from "@expo/vector-icons/Octicons";
+import { Tabs } from "expo-router";
+import React from "react";
+
+export default function TabLayout() {
+  const { bottom } = useSafeArea();
+  return (
+    <React.Fragment>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "#A2A77F",
+          tabBarInactiveTintColor: "grey",
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            paddingTop: 10,
+            paddingBottom: bottom,
+            backgroundColor: "#EFF1C5",
+            borderTopWidth: 0,
+          },
+          headerShadowVisible: false,
+        }}
+      >
+        <Tabs.Screen
+          name="simulatorScreen"
+          options={{
+            tabBarIcon: (props) => (
+              <Octicons
+                name={props.focused ? "home-fill" : "home"}
+                size={24}
+                color={props.color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="compareScreen"
+          options={{
+            tabBarIcon: ({ color, focused }) => (
+              <Octicons
+                name={focused ? "git-compare" : "git-compare"}
+                size={24}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="historyScreen"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Octicons name={"history"} size={24} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </React.Fragment>
+  );
+}
