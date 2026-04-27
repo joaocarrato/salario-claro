fc38af773aad4d5ed7cf696f0de5f1b1729611d2
joaocarrato <joaocarrato18@gmail.com>
2026-04-27 18:32:06 -0300

    refactor: move tab navigation into root layout

---
+++ app/SimulatorScreen.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function SimulatorScreen() {
+    return (
+        <View className="flex-1 justify-center items-center px-5 bg-background">
+            <Text className="text-3xl color-text-primary">Simulator Screen</Text>
+        </View>
+    )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,65 @@
 import "@/global.css";
 import { useSafeArea } from "@/src/hooks/useSafeArea";
+import Octicons from "@expo/vector-icons/Octicons";
+import { Tabs } from "expo-router";
 import React from "react";
 import { StatusBar } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import TabLayout from "./(tabs)";
 
 export default function RootLayout() {
   const { bottom } = useSafeArea();
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={"default"} />
-      <TabLayout />
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "#A2A77F",
+          tabBarInactiveTintColor: "grey",
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            paddingTop: 10,
+            paddingBottom: bottom,
+            backgroundColor: "#EFF1C5",
+            borderTopWidth: 0,
+            elevation: 0,
+          },
+          headerShadowVisible: false
+        }}
+      >
+        <Tabs.Screen
+          name="SimulatorScreen"
+          options={{
+            tabBarIcon: (props) => (
+              <Octicons
+                name={props.focused ? "home-fill" : "home"}
+                size={24}
+                color={props.color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="CompareScreen"
+          options={{
+            tabBarIcon: ({ color, focused }) => (
+              <Octicons
+                name={focused ? "git-compare" : "git-compare"}
+                size={24}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="HistoryScreen"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Octicons name={"history"} size={24} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
     </SafeAreaProvider>
   );
 }
