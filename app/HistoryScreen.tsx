ca999830dae0686606a18f02c089bf6f1a60f2f6
joaocarrato <joaocarrato18@gmail.com>
2026-05-11 21:26:49 -0300

    feat: implement HistoryCard component and update HistoryScreen layout

--- app/HistoryScreen.tsx
+++ app/HistoryScreen.tsx
@@ -1,3 +1,4 @@
+import { HistoryCard } from "@/src/components/HistoryCard/HistoryCard";
 import Screen from "@/src/components/Screen/Screen";
 import React from "react";
 import { Text } from "react-native";
@@ -5,7 +6,12 @@ import { Text } from "react-native";
 export default function HistoryScreen() {
   return (
     <Screen>
-      <Text>History Screen</Text>
+      <Text className="text-4xl font-roboto-bold">Histórico</Text>
+      <Text className="text-lg font-roboto color-subtitle mt-2 mb-8">
+        Suas ultimas simulações
+      </Text>
+
+      <HistoryCard />
     </Screen>
   );
 }
---
+++ src/components/HistoryCard/HistoryCard.tsx
@@ -0,0 +1,43 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import React from "react";
+import { Pressable, Text, View } from "react-native";
+
+export function HistoryCard() {
+  return (
+    <View className="bg-white rounded-lg p-6 shadow-sm">
+      <View className="flex-row items-center">
+        <View className="flex-row items-center gap-2">
+          <Ionicons name="time" color={"#00685F"} size={28} />
+          <Text className="text-lg font-roboto-bold text-primary">
+            ULTIMA SIMULAÇÃO
+          </Text>
+        </View>
+        <Text className="text-sm font-roboto color-subtitle ml-auto">
+          Hoje, 14:30
+        </Text>
+      </View>
+
+      <Text className="text-lg font-roboto color-subtitle mt-4">
+        Salário líquido estimado
+      </Text>
+
+      <Text className="text-4xl font-roboto-bold mt-1">R$ 5.000,00</Text>
+
+      <Divider />
+
+      <Text className="text-lg font-roboto color-subtitle">Salário bruto</Text>
+      <Text className="text-2xl font-roboto-bold mt-1 color-secondary">
+        R$ 5.000,00
+      </Text>
+
+      <Pressable className="self-end flex-row items-center gap-1">
+        <Text className="text-primary font-roboto-bold ">Ver detalhes</Text>
+        <Ionicons name="arrow-forward" size={18} color={"#00685F"} />
+      </Pressable>
+    </View>
+  );
+}
+
+function Divider() {
+  return <View className="h-0.5 bg-gray-200 my-6" />;
+}
